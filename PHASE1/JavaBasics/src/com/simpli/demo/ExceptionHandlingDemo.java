145262adb435986383b8501744ebed68abd1a5f6
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2023-12-27 12:03:26 +0530

    update

--- PHASE1/JavaBasics/src/com/simpli/demo/ExceptionHandlingDemo.java
+++ PHASE1/JavaBasics/src/com/simpli/demo/ExceptionHandlingDemo.java
@@ -22,21 +22,29 @@ public class ExceptionHandlingDemo {
 
 		System.out.println(" So here size of each plot after division " + plotSize / noOfPiecesToDivide);
 
-		
-
+		FileReader f = null;
 		String myFile = "C:\\Temp\\log.txt";
 		try {
-			FileReader f = new FileReader(myFile);
+			f = new FileReader(myFile);
 			f.read();
-			
+
 		} catch (FileNotFoundException e) {
 			System.out.printf("Given file %s not found in the system.  \n"
-					+ " Make sure it is in the said location and then retry\\n",myFile);
+					+ " Make sure it is in the said location and then retry\\n", myFile);
+
 		} catch (IOException e) {
 			System.out.printf("Given file %s is not read\n"
-					+ " Make sure it has read permission for your account and then retry\\n",myFile);
+					+ " Make sure it has read permission for your account and then retry\\n", myFile);
+
+		} finally {
+			// always gets executed
+			try {
+				f.close();
+			} catch (IOException e) {
+
+			}
 		}
-		
+
 		System.out.println(" Thank you ");
 
 	}
