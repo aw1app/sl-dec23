ab785b103c9d45414caa256513e93bf7dba43ddd
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-03 11:33:27 +0530

    update

---
+++ PHASE1/JavaBasics/src/com/simpli/demo/dsa/search/LineraSearchDemo.java
@@ -0,0 +1,42 @@
+package com.simpli.demo.dsa.search;
+
+public class LineraSearchDemo {
+
+	public static void main(String[] args) {
+
+		int arr[] = { 10, 20, 30, 23, 40, 50, 77 };
+
+		int key = 40;
+
+		int positionOfKey = doLinearSearch(arr, key);
+
+		if (positionOfKey == -1)
+			System.out.println("Key " + key + " not found ");
+		
+		
+		key = 41;
+
+		positionOfKey = doLinearSearch(arr, key);
+
+		if (positionOfKey == -1)
+			System.out.println("Key " + key + " not found ");
+
+	}
+
+	private static int doLinearSearch(int[] arr, int key) {
+		int positionOfKey = -1;
+
+		for (int i = 0; i < arr.length; i++) {
+
+			if (arr[i] == key) {
+				System.out.println("Found the key " + key + " at position " + i);
+				positionOfKey = i;
+				break;
+			}
+		}
+	
+		return positionOfKey;
+
+	}
+
+}
