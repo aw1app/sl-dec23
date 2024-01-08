dd98660c11be26fe97077ca8679e3edb42a826f2
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-08 12:48:23 +0530

    update

--- PHASE2/ServletDemo/src/main/java/com/ecommerce/FormProcessingServlet.java
+++ PHASE2/ServletDemo/src/main/java/com/ecommerce/FormProcessingServlet.java
@@ -2,6 +2,7 @@ package com.ecommerce;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -22,10 +23,14 @@ public class FormProcessingServlet extends HttpServlet {
 		
 		String userid = request.getParameter("userid");
 		String pass = request.getParameter("pass");
+		String gender = request.getParameter("gender");
+		
+		String[] hobbies = request.getParameterValues("hobbies");
 		
 		out.write("Hey user, got your GET request");
 		out.write("<br>  You submitted userid=<b>"+userid + "</b> and  your password is "+ pass);
-		
+		out.write("<br> Gender="+gender + "Hobbies=" + Arrays.toString(hobbies));
+				
 		System.out.printf("\n User-Agent header value = %s \n", request.getHeader("User-Agent"));
 		
 		out.close();
