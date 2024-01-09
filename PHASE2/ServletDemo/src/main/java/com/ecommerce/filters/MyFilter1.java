6964bb965e89a7b7a635ae911634cfea3d2d5f85
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-09 10:32:35 +0530

    update

--- PHASE2/ServletDemo/src/main/java/com/ecommerce/filters/MyFilter1.java
+++ PHASE2/ServletDemo/src/main/java/com/ecommerce/filters/MyFilter1.java
@@ -1,24 +1,27 @@
 package com.ecommerce.filters;
 
-import jakarta.servlet.Filter;
-import jakarta.servlet.http.HttpFilter;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import java.io.PrintWriter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
 
 public class MyFilter1 extends HttpFilter implements Filter {
-
 	
 	public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+		System.out.println("Inside MyFilter1");
 		
+		PrintWriter out = response.getWriter();
+		
+		String comments= request.getParameter("comments");
+		
+		if(comments==null || comments.equals("")) {
+			out.print("Comments section cannot be null. Please enter some comments.");
+		}
+		else {
 		// pass the request along the filter chain
 		chain.doFilter(request, response);
+		}
 	}
 
 	
