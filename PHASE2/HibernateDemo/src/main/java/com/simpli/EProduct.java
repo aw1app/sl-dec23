4887fff401d2e9faa110b52242e0b7b703597c4a
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-12 10:48:41 +0530

    update

--- PHASE2/HibernateDemo/src/main/java/com/simpli/EProduct.java
+++ PHASE2/HibernateDemo/src/main/java/com/simpli/EProduct.java
@@ -9,5 +9,31 @@ public class EProduct {
      private String name;
      private BigDecimal price;
      private Date dateAdded;
+	public long getID() {
+		return ID;
+	}
+	public void setID(long iD) {
+		ID = iD;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public BigDecimal getPrice() {
+		return price;
+	}
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	public Date getDateAdded() {
+		return dateAdded;
+	}
+	public void setDateAdded(Date dateAdded) {
+		this.dateAdded = dateAdded;
+	}
+     
+     
 
 }
