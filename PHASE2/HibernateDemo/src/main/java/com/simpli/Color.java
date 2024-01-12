ba4417df9d0df85ea72d36ae336c87755a2541a3
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-12 13:35:20 +0530

    update

---
+++ PHASE2/HibernateDemo/src/main/java/com/simpli/Color.java
@@ -0,0 +1,24 @@
+package com.simpli;
+
+public class Color {
+	
+	private long COLORID;
+	private String name;
+	
+	public long getCOLORID() {
+		return this.COLORID;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setCOLORID(long id) {
+		this.COLORID = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
