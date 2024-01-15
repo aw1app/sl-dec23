8fc6f21a266cf806793f82de67ca2be6f03daab1
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-15 10:20:17 +0530

    update

---
+++ PHASE2/HibernateDemo/src/main/java/com/simpli/OS.java
@@ -0,0 +1,22 @@
+package com.simpli;
+
+public class OS {
+	
+	private long OSID;
+    private String name;
+    
+    
+	public long getOSID() {
+		return OSID;
+	}
+	public void setOSID(long oSID) {
+		OSID = oSID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}    
+
+}
