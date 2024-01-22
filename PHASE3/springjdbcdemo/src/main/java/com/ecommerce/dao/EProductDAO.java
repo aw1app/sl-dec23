24a24b4942d08b11bf5952ca962c5d6b9dcb88f5
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-22 11:52:58 +0530

    update

---
+++ PHASE3/springjdbcdemo/src/main/java/com/ecommerce/dao/EProductDAO.java
@@ -0,0 +1,42 @@
+package com.ecommerce.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import com.ecommerce.EProduct;
+
+public class EProductDAO {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	// Business method
+	// 1. Fetch product objects from the DB
+
+	public List<EProduct> getProducts() {
+		jdbcTemplate.query("SELECT * FROM eproduct", new EProductRowMapper());
+	}
+
+}
+
+class EProductRowMapper implements RowMapper<EProduct> {
+
+	@Override
+	public EProduct mapRow(ResultSet rs, int rowNum) throws SQLException {
+		
+		EProduct eProduct = new EProduct();
+		
+		eProduct.setID(rs.getInt(1));
+		eProduct.setName(rs.getString(2));
+		eProduct.setPrice(rs.getBigDecimal(3));
+		eProduct.setDateAdded(rs.getDate(4));
+
+		return eProduct;
+	}
+
+}
