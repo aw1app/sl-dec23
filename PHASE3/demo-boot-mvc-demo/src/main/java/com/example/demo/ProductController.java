c841a0c5d33cfbef766bf74b7a10a37dbd115048
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-23 12:11:56 +0530

    update

--- PHASE3/demo-boot-mvc-demo/src/main/java/com/example/demo/ProductController.java
+++ PHASE3/demo-boot-mvc-demo/src/main/java/com/example/demo/ProductController.java
@@ -1,12 +1,31 @@
 package com.example.demo;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ecommerce.entity.EProduct;
+import com.ecommerce.repositry.EProductRepositry;
 
 @Controller
 public class ProductController {
 	
-	//@GetMapping("listProduct")
+	@Autowired
+	EProductRepositry eProductRepositry;
+	
+	@GetMapping("/listProducts")
+	public String listProducts(ModelMap model) {
+		
+		List<EProduct>  listOfProducts = eProductRepositry.findAll();
+		
+		model.addAttribute("prodlist" , listOfProducts);
+		
+		return "list-of-products"; // go to list-of-products.jsp
+		
+	}
 	
 
 }
