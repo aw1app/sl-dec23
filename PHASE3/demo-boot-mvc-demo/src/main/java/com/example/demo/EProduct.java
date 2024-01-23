05b30775c0d3ff9389aa208e129f19e329c127a0
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-23 12:03:11 +0530

    update

---
+++ PHASE3/demo-boot-mvc-demo/src/main/java/com/example/demo/EProduct.java
@@ -0,0 +1,69 @@
+package com.example.demo;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "eproduct")
+public class EProduct {
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "ID")
+	private long ID;
+	
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "price")
+	private BigDecimal price;
+
+	@Column(name = "date_added")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateAdded;
+
+	public long getID() {
+		return ID;
+	}
+
+	public void setID(long iD) {
+		ID = iD;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public Date getDateAdded() {
+		return dateAdded;
+	}
+
+	public void setDateAdded(Date dateAdded) {
+		this.dateAdded = dateAdded;
+	}
+	
+	
+	
+	
+
+}
