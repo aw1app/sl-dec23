769f1ab6d2ec21e869110579f8a1f16828a316f1
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-25 12:44:40 +0530

    update

--- PHASE3/springhibernatedemo/src/main/java/com/ecommerce/controller/EProductController.java
+++ PHASE3/springhibernatedemo/src/main/java/com/ecommerce/controller/EProductController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ecommerce.dao.EProductDAO;
 import com.ecommerce.entity.EProductEntity;
@@ -27,6 +29,25 @@ public class EProductController {
 		model.addAttribute("prodlist", listOfProducts);
 
 		return "list-of-products"; // go to list-of-products.jsp
+
+	}
+	
+	
+	@PostMapping("/addProduct")
+	public String addProduct(ModelMap model, @RequestParam String name,@RequestParam float price) {
+
+		eProductDAO.addProduct(name, price);
+		
+		return "add-product-success";
+
+	}
+	
+	@GetMapping("/deleteProduct")
+	public String deleteProduct(ModelMap model, @RequestParam long id) {
+
+		eProductDAO.deleteProduct(id);
+		
+		return "delete-product-success";
 
 	}
 
--- PHASE3/springhibernatedemo/src/main/java/com/ecommerce/dao/EProductDAO.java
+++ PHASE3/springhibernatedemo/src/main/java/com/ecommerce/dao/EProductDAO.java
@@ -1,8 +1,10 @@
 package com.ecommerce.dao;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -24,6 +26,25 @@ public class EProductDAO {
 		Query query = this.sessionFactory.getCurrentSession().createQuery("from EProductEntity");
 		
 		return query.list();
+	}
+	
+	// Add a product
+	
+	public void addProduct(String name, float price) {
+		EProductEntity eProduct = new EProductEntity();
+		eProduct.setName(name);
+		eProduct.setPrice(new BigDecimal(price));		
+		
+		this.sessionFactory.getCurrentSession().save(eProduct);		
+	}
+	
+	// Delete a product
+	public void deleteProduct(long id) {
+		Session session = this.sessionFactory.getCurrentSession();
+		
+		EProductEntity eProduct = (EProductEntity) session.load(EProductEntity.class, id);
+		
+		session.delete(eProduct);
 	}
 
 }
