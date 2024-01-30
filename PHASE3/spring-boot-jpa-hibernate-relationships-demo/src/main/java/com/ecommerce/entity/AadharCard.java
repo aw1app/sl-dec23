f078880f3f012d99ed30c64c2ec5827cc2631fa3
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-01-30 12:11:16 +0530

    update

--- PHASE3/spring-boot-jpa-hibernate-relationships-demo/src/main/java/com/ecommerce/entity/AadharCard.java
+++ PHASE3/spring-boot-jpa-hibernate-relationships-demo/src/main/java/com/ecommerce/entity/AadharCard.java
@@ -1,5 +1,33 @@
 package com.ecommerce.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "aadhar")
 public class AadharCard {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private long ID;
+
+	// @Pattern(regexp = "\\d{12}")
+	private long number;
+
+	public long getID() {
+		return ID;
+	}
+
+	public void setID(long iD) {
+		ID = iD;
+	}
+
+	public long getNumber() {
+		return number;
+	}
+
+	public void setNumber(long number) {
+		this.number = number;
+	}
 
 }
