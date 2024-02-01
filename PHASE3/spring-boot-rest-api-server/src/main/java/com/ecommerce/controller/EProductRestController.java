f45fec0d6b570e5710ec575dcd05db5316dc03ef
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-02-01 10:22:36 +0530

    update

--- PHASE3/spring-boot-rest-api-server/src/main/java/com/ecommerce/controller/EProductRestController.java
+++ PHASE3/spring-boot-rest-api-server/src/main/java/com/ecommerce/controller/EProductRestController.java
@@ -1,9 +1,11 @@
 package com.ecommerce.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,19 +15,33 @@ import com.ecommerce.repositry.EProductRepositry;
 @RestController
 @RequestMapping("/product")
 public class EProductRestController {
-	
+
 	@Autowired
 	EProductRepositry eProductRepositry;
-	
+
 	// Fetch full list of products from the DB
 	@GetMapping("/list")
 	public List<EProduct> showAllProducts() {
 
 		List<EProduct> products = eProductRepositry.findAll();
-		
+
 		return products;
 	}
-	
 
+	// Fetch a Single Product Details from the DB
+	@GetMapping(value = "/details/{id}")
+	public EProduct showProduct(@PathVariable("id") long id) {
+
+		Optional<EProduct> productFromRepo = eProductRepositry.findById(id);
+
+		if (productFromRepo.isPresent()) {
+			EProduct product = productFromRepo.get();
+			return product;
+		} else {
+			return new EProduct();
+			// return "Product with id = "+ id + " not found";
+		}
+
+	}
 
 }
