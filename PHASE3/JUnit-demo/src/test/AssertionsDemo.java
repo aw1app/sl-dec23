f32e7abf8fcd98557084e97803458557af10dfe5
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-02-02 12:25:01 +0530

    update

--- PHASE3/JUnit-demo/src/test/AssertionsDemo.java
+++ PHASE3/JUnit-demo/src/test/AssertionsDemo.java
@@ -8,40 +8,54 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import com.ecommerce.Calculator;
+
 public class AssertionsDemo {
-	
+
 	@Test
-	void test() {		
-		
+	void test() {
+
 		String str = null;
 		String str2 = "some value";
-		
+
 		String[] a1 = { "A", "B" };
 		String[] a2 = { "A", "B" };
-		
-		int a=4;
-		int b=0;
-		
-		assertTrue(a > b);		
+
+		int a = 4;
+		int b = 0;
+
+		assertTrue(a > b);
 		assertFalse(5 < 1);
-		
-		assertNull(str);		
+
+		assertNull(str);
 		assertNotNull(str2);
-		
-		
+
 		assertSame(str, str);
 		assertNotSame(str, str2);
-		
+
 		assertEquals(5, 5);
-        assertNotEquals(5, 6);
-        
-        assertArrayEquals(a1, a2);
+		assertNotEquals(5, 6);
+
+		assertArrayEquals(a1, a2);
+
+		assertThrows(RuntimeException.class, () -> {
+			throw new RuntimeException();
+		});
 		
+//		assertThrows(ArithmeticException.class, () -> {
+//			throw new RuntimeException();
+//		});
 		
+		assertThrows(ArithmeticException.class, () -> {
+			Calculator calculator = new Calculator();
+			calculator.divide(10, 0);
+		});
+
 	}
 
 }
