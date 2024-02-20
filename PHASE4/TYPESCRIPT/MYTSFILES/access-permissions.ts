a25fe29fd4370344c6790dcb5451b00c23598433
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-02-20 12:44:43 +0530

    update

---
+++ PHASE4/TYPESCRIPT/MYTSFILES/access-permissions.ts
@@ -0,0 +1,28 @@
+class Bus {
+
+    name: string;
+    private mileage: number;
+    protected age: number;
+    readonly engineType: string;
+
+    constructor(name: string, mileage: number, age: number, engineType: string) {
+        this.name = name;
+        this.mileage = mileage;
+        this.age = age;
+        this.engineType = engineType;
+    }
+
+    showMileage = (): number => this.mileage;
+
+}
+
+let bus1:Bus = new Bus("Volvo", 100, 90, "petrol");
+
+// bus1.name // GOOD
+// bus1.mileage // ERROR
+
+// bus1.engineType // GOOD
+// bus1.engineType = "Gas" // ERROR
+
+//console.log(`Mileage is ${bus1.mileage}`); // ERROR mileage is private
+console.log(`Mileage is ${bus1.showMileage()}`);
