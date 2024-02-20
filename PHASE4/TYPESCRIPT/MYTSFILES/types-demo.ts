b6fe09756140448a37fe89a527b66aa530be5699
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-02-20 12:03:28 +0530

    update

--- PHASE4/TYPESCRIPT/MYTSFILES/types-demo.ts
+++ PHASE4/TYPESCRIPT/MYTSFILES/types-demo.ts
@@ -29,11 +29,27 @@ let car1: Car = new Car("Toyota", 70);
 
 console.log(`Car 1 name is ${car1.name} and mileage is ${car1.mileage}`);
 
-
+// Demo Using classes that were defined in some other file
 import { Person } from './interface-demo';
 
 let per10: Person = new Person("Mr President");
 per10.walkFast();
+
+// Array
+let myArr1 = []; // in Plain JS
+
+let myArr2 : string[] = []; // in TS
+
+let carsArr: Car[] = [];
+carsArr[0] = new Car("Ford",100);
+carsArr[1] = new Car("Maruti",150);
+
+carsArr.push(new Car("Fiat",250));
+
+console.log(`\n Array type Demo \n ${carsArr}`);
+console.log(`\n Array type Demo \n ${carsArr[0].name}`);
+
+
 
 
 
