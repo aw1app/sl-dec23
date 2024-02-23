06f14052390417ee65f7c5daa6636ea7251518c9
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-02-23 12:16:45 +0530

    update

--- PHASE4/ANGULAR/my_first_angular_project/src/app/app.component.ts
+++ PHASE4/ANGULAR/my_first_angular_project/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductComponent } from './product/product.component';
 import { ProductlistComponent } from './productlist/productlist.component';
+import { ProductV1Component } from './product-v1/product-v1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductComponent, ProductlistComponent],
+  imports: [RouterOutlet, ProductComponent, ProductlistComponent, ProductV1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
