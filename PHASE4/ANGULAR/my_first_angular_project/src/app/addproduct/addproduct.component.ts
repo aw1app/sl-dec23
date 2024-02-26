44c88117e28f6c2261e8606b11bb685f506748e2
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-02-26 12:04:11 +0530

    update

--- PHASE4/ANGULAR/my_first_angular_project/src/app/addproduct/addproduct.component.ts
+++ PHASE4/ANGULAR/my_first_angular_project/src/app/addproduct/addproduct.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+// Required for form(reactive) processing 
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'addproduct',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './addproduct.component.html',
   styleUrl: './addproduct.component.css'
 })
 export class AddproductComponent {
+
+  productForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){
+
+    this.productForm = this.fb.group(
+      {
+        productName: ['', [Validators.required, Validators.minLength(3)]],       
+      }
+    ); 
+
+  }
+
+
 
 }
