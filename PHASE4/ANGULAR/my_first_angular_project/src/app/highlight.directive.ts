cffef102acae4a74eb015965eccddc1f0d97505c
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-02-26 13:07:43 +0530

    update

--- PHASE4/ANGULAR/my_first_angular_project/src/app/highlight.directive.ts
+++ PHASE4/ANGULAR/my_first_angular_project/src/app/highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]',
@@ -6,7 +6,7 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 })
 export class HighlightDirective {
 
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
 
   @HostListener('mouseenter') onMouseEnter() {
     this.highlight('yellow');
@@ -14,14 +14,20 @@ export class HighlightDirective {
 
   @HostListener('mouseleave') onMouseLeave() {
     this.highlight('lightgrey');
+    this.changeFontSize('12px');
   }
 
   @HostListener('click') onMouseClick() {
-    this.highlight('green');
+    this.highlight('lightgreen');
+    this.changeFontSize('48px');
   }
 
   private highlight(color: string | null) {
     this.el.nativeElement.style.backgroundColor = color;
+  }
+
+  private changeFontSize(size: string | "12px") {
+    this.renderer.setStyle(this.el.nativeElement, 'font-size', size);
   }
 
 }
