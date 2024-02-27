00a3efa0e8fdf36eb602faa6a68a1b68b5846483
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-02-27 10:34:39 +0530

    update

---
+++ PHASE4/ANGULAR/angular_project_with_service_routing/src/app/addproduct/addproduct.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ProductService } from '../service/product.service';
+
+@Component({
+  selector: 'addproduct',
+  standalone: true,
+  imports: [FormsModule,ReactiveFormsModule],
+  templateUrl: './addproduct.component.html',
+  styleUrl: './addproduct.component.css'
+})
+export class AddproductComponent {
+
+  productForm!: FormGroup;
+  productService!: ProductService;
+  fb!: FormBuilder;
+
+  constructor(productService: ProductService, fb: FormBuilder) {
+    this.productService = productService;
+
+    this.productForm = this.fb.group(
+      {
+        productName: ['', [Validators.required, Validators.minLength(3)]],
+        productPrice: ['', [Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/), Validators.max(100000)]]
+      }
+    );
+  }
+
+  addProduct= ():number =>  this.productService.addProduct(this.productForm.value.productName,this.productForm.value.productPrice);
+
+}
--- PHASE4/ANGULAR/angular_project_with_service_routing/src/app/app.component.ts
+++ PHASE4/ANGULAR/angular_project_with_service_routing/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductlistComponent } from './productlist/productlist.component';
+import { AddproductComponent } from './addproduct/addproduct.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductlistComponent],
+  imports: [RouterOutlet, ProductlistComponent, AddproductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
