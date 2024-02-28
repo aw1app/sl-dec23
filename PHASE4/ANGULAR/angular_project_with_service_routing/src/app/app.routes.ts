9f8a0e93d4eb5b16f2d871ff372bbd67ac1f229f
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-02-28 10:05:03 +0530

    update

--- PHASE4/ANGULAR/angular_project_with_service_routing/src/app/app.routes.ts
+++ PHASE4/ANGULAR/angular_project_with_service_routing/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AddproductComponent } from './addproduct/addproduct.component';
+import { ProductlistV1Component } from './productlist-v1/productlist-v1.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: 'add-product', component: AddproductComponent },
+    { path: 'list-products', component: ProductlistV1Component },
+
+
+];
