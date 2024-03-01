8094bae300330cda7c1395bd6d9c707daacebbf5
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-03-01 10:58:58 +0530

    update

--- PHASE5/hello-selenium/src/main/java/com/sl/LocateElementsDemo.java
+++ PHASE5/hello-selenium/src/main/java/com/sl/LocateElementsDemo.java
@@ -3,26 +3,21 @@ package com.sl;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class HelloWorldSelenium {
-	
-	
+public class LocateElementsDemo {
+
+	static WebDriver driver = null;
 
 	public static void main(String[] args) throws InterruptedException {
+
+		driver = new FirefoxDriver();
+
 		
-		// WebDriver driver = new ChromeDriver(); // launch Chrome Browser
 		
-		WebDriver driver = new FirefoxDriver(); // launch FF Browser
 		
-		String baseUrl ="https://www.google.com";
-				
-		driver.get(baseUrl);		
 		
 		Thread.sleep(10000);// 10 secs sleep
-		
 		driver.close(); // close the automated browser
 
 	}
 
 }
-
-
