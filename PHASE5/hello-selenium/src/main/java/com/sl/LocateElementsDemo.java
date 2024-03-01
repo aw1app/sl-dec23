7d8d0345f406e54a05f352de8b16757acc5b847a
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-03-01 11:31:22 +0530

    update

--- PHASE5/hello-selenium/src/main/java/com/sl/LocateElementsDemo.java
+++ PHASE5/hello-selenium/src/main/java/com/sl/LocateElementsDemo.java
@@ -1,7 +1,9 @@
 package com.sl;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LocateElementsDemo {
 
@@ -9,13 +11,21 @@ public class LocateElementsDemo {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		driver = new FirefoxDriver();
+		driver = new ChromeDriver();
 
+		String baseUrl = "https://www.facebook.com/r.php?locale=en_GB&display=page";
+		
+		driver.get(baseUrl);
+		
+		//WebElement firstNameElement = driver.findElement(By.id("u_0_b_QU"));
+		//above did not work as facebook is creating the id dynamically (that is it not  "u_0_b_QU" always)
+		
+		// so try another approach
+		WebElement firstNameElement = driver.findElement(By.name("firstname"));
+		firstNameElement.sendKeys("abcdef");
 		
 		
-		
-		
-		Thread.sleep(10000);// 10 secs sleep
+		Thread.sleep(20000);// 20 secs sleep
 		driver.close(); // close the automated browser
 
 	}
