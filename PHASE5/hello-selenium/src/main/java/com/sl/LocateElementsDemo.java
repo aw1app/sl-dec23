9d925e0e3346b83b3b95db409e9551b0317de46e
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-03-01 11:45:23 +0530

    update

--- PHASE5/hello-selenium/src/main/java/com/sl/LocateElementsDemo.java
+++ PHASE5/hello-selenium/src/main/java/com/sl/LocateElementsDemo.java
@@ -13,20 +13,47 @@ public class LocateElementsDemo {
 
 		driver = new ChromeDriver();
 
+		// faceBookAcctCreation();
+
+		googleNewAccCreation();
+
+		Thread.sleep(30000);// 30 secs sleep
+		driver.close(); // close the automated browser
+
+	}
+
+	public static void faceBookAcctCreation() {
+
 		String baseUrl = "https://www.facebook.com/r.php?locale=en_GB&display=page";
-		
+
 		driver.get(baseUrl);
-		
-		//WebElement firstNameElement = driver.findElement(By.id("u_0_b_QU"));
-		//above did not work as facebook is creating the id dynamically (that is it not  "u_0_b_QU" always)
-		
+
+		// Locate by id
+		// WebElement firstNameElement = driver.findElement(By.id("u_0_b_QU"));
+		// above did not work as facebook is creating the id dynamically (that is it not
+		// "u_0_b_QU" always)
+
 		// so try another approach
+		// Locate by name
 		WebElement firstNameElement = driver.findElement(By.name("firstname"));
 		firstNameElement.sendKeys("abcdef");
-		
-		
-		Thread.sleep(20000);// 20 secs sleep
-		driver.close(); // close the automated browser
+	}
+
+	// google New Acc Creation
+	public static void googleNewAccCreation() {
+
+		String baseUrl = "https://accounts.google.com/signup/v2/createaccount?flowEntry=SignUp";
+
+		driver.get(baseUrl);
+
+		// Locate firstname textbox by its id
+		WebElement firstNameTF = driver.findElement(By.id("firstName"));
+
+		firstNameTF.sendKeys("Krishna");
+
+		// Locate the last name feild using name
+		WebElement lastNameTF = driver.findElement(By.id("lastName"));
+		lastNameTF.sendKeys("Varma");
 
 	}
 
