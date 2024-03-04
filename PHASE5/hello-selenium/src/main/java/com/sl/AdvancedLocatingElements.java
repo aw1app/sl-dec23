567e6e5368d260b3f605e30116bc02e581acb8a1
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-03-04 09:56:36 +0530

    update

--- PHASE5/hello-selenium/src/main/java/com/sl/AdvancedLocatingElements.java
+++ PHASE5/hello-selenium/src/main/java/com/sl/AdvancedLocatingElements.java
@@ -1,6 +1,7 @@
 package com.sl;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -17,11 +18,16 @@ public class AdvancedLocatingElements {
 
 		driver = new ChromeDriver();
 		
-		Thread.sleep(10000);// 10 secs delay for instructor to share the screen
+		// Set the page load timeout to 30 seconds
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 		
-		waitDemo();
+		Thread.sleep(10000);// 10 secs delay 
 		
-		//driver.close();
+		//waitDemo();
+		
+		locateMobileTF();
+		
+		driver.close();
 
 	}
 
@@ -36,6 +42,17 @@ public class AdvancedLocatingElements {
 		WebElement firstNameElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("firstname")));
 		
 		firstNameElement.sendKeys("abcdef");
+
+	}
+	
+	public static void locateMobileTF() {
+		String baseUrl = "https://www.facebook.com/r.php?locale=en_GB&display=page";
+
+		driver.get(baseUrl);
+		
+		WebElement mobileNoTF = driver.findElement(By.cssSelector("input[]"));
+		
+		mobileNoTF.sendKeys("93949499323");
 
 	}
 
