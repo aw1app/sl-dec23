b32a0979031deecde9b49a863d2159f097f13fc2
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-03-05 11:58:05 +0530

    update

--- PHASE5/hello-selenium-testng/src/main/java/com/sl/GoogleSearch.java
+++ PHASE5/hello-selenium-testng/src/main/java/com/sl/GoogleSearch.java
@@ -1,7 +1,7 @@
 package com.sl;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -10,15 +10,18 @@ public class GoogleSearch {
 	// public WebDriver driver = new FirefoxDriver();
 
 	static {
+		// For Chrome Driver only
 		// Downlaod from
 		// https://storage.googleapis.com/chrome-for-testing-public/118.0.5962.0/win64/chromedriver-win64.zip
 		// and upzip
-
-		String chromeDriverLocation = "C:\\Users\\home\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe";
+		
+		String chromeDriverLocation = "C:\\Users\\home\\Downloads\\chromedriver-win64v122\\chromedriver-win64\\chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", chromeDriverLocation);
+		
 	};
 
-	WebDriver driver = new ChromeDriver();
+	//WebDriver driver = new ChromeDriver();
+	WebDriver driver = new FirefoxDriver();
 
 	SoftAssert softAssert = new SoftAssert();
 
@@ -28,6 +31,18 @@ public class GoogleSearch {
 		driver.get("http://www.google.com");
 
 		System.out.println("Title of google page is " + driver.getTitle());
+
+		softAssert.assertEquals(driver.getTitle(), "Google");
+
+	}
+	
+	// Task Check of Google search text feild appears and contains text "flowers"
+	@Test
+	public void searchGoogleAndTestSearchTextFeildContainsFlowers() {
+
+		driver.get("http://www.google.com?q=flowers");
+
+		System.out.println("text feild contains text \"flowers\" ");
 
 		softAssert.assertEquals(driver.getTitle(), "Google");
 
