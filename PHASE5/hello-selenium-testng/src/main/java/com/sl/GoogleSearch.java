a7451cb66d8707ea9cd5177e4334fbd848c98ff6
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-03-05 11:29:17 +0530

    update

--- PHASE5/hello-selenium-testng/src/main/java/com/sl/GoogleSearch.java
+++ PHASE5/hello-selenium-testng/src/main/java/com/sl/GoogleSearch.java
@@ -1,5 +1,24 @@
 package com.sl;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
 public class GoogleSearch {
+	
+	public WebDriver driver;
+	SoftAssert softAssert = new SoftAssert();
+	
+	@Test
+	public void searchGoogleAndTestTitleText() {
+		
+		driver.get("http://www.google.com");
+		
+		System.out.println("Title of google page is " + driver.getTitle());
+		
+		softAssert.assertEquals(driver.getTitle(), "Google");		
+		
+	}
+
 
 }
