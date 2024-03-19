a5583c2bfe70f02af65f0662d89fa901c3d639cf
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2024-03-19 10:45:39 +0530

    update

--- PHASE3/spring-boot-rest-api-server/src/main/java/com/ecommerce/controller/EProductRestController.java
+++ PHASE3/spring-boot-rest-api-server/src/main/java/com/ecommerce/controller/EProductRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,8 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ecommerce.entity.EProduct;
 import com.ecommerce.repositry.EProductRepositry;
 
+
 @RestController
 @RequestMapping("/product")
+@CrossOrigin
 public class EProductRestController {
 
 	@Autowired
